46c1b608dfe5ceb80d46dc8c312a28bd5fbfecf2
Viktor Sysyuk <s.vik.json@gmail.com>
2020-08-11 18:28:15 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store';
 
 
-    ReactDOM.render( <HashRouter>
+    ReactDOM.render( <BrowserRouter>
         <Provider store={store}>
           <App />
         </Provider>
-      </HashRouter>, document.getElementById('root'));
+      </BrowserRouter>, document.getElementById('root'));
 
 
 
