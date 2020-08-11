ea71621d1a46a171b6acb9fdf1e9da8874378f86
Viktor Sysyuk <s.vik.json@gmail.com>
2020-08-11 13:59:27 +0300

    Use HashRouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store';
 
 
-    ReactDOM.render( <BrowserRouter>
+    ReactDOM.render( <HashRouter>
         <Provider store={store}>
           <App />
         </Provider>
-      </BrowserRouter>, document.getElementById('root'));
+      </HashRouter>, document.getElementById('root'));
 
 
 
