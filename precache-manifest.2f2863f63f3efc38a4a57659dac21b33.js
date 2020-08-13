c06e4064745e47b01da38b6b1154f4850b7cf859
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-13 15:36:02 +0000

    Deploy s-vik/demo-social-net to github.com/s-vik/demo-social-net.git:gh-pages

---
+++ precache-manifest.2f2863f63f3efc38a4a57659dac21b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf450be247862338ceeb46cc78a2a40c",
+    "url": "/demo-social-net/index.html"
+  },
+  {
+    "revision": "8b7552d5b9c32f6deb7c",
+    "url": "/demo-social-net/static/css/main.db031ed9.chunk.css"
+  },
+  {
+    "revision": "713615870fc25e3b6d15",
+    "url": "/demo-social-net/static/js/2.8af2ec7e.chunk.js"
+  },
+  {
+    "revision": "8b7552d5b9c32f6deb7c",
+    "url": "/demo-social-net/static/js/main.b3802d6c.chunk.js"
+  },
+  {
+    "revision": "945af7f977eee158f3d4",
+    "url": "/demo-social-net/static/js/runtime-main.425f77ff.js"
+  },
+  {
+    "revision": "634e55429bf1a43f33f35ea5cb34a3b3",
+    "url": "/demo-social-net/static/media/preloader.634e5542.svg"
+  }
+]);
